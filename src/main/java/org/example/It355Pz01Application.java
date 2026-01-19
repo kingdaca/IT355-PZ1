019521bbf788a263626513e2c218fc7822934875
David Faks <davidfaks@Davids-MacBook-Pro.local>
2026-01-19 15:22:33 +0100

    first commit

---
+++ src/main/java/org/example/It355Pz01Application.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It355Pz01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(It355Pz01Application.class, args);
+    }
+
+}
